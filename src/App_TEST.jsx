f1fb964ee2ff3a2670d8a200a5f5f53f07ddf899
sethu1286-jpg <sethu1286@gmail.com>
2026-05-03 04:30:45 +0530

    Add files via upload

---
+++ src/App_TEST.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#0F1B3D",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "sans-serif",
+      color: "white",
+      padding: "24px",
+      textAlign: "center",
+    }}>
+      <div style={{ fontSize: 60, marginBottom: 20 }}>🌞</div>
+      <div style={{ fontSize: 28, fontWeight: 900, color: "#FF8C00", marginBottom: 12 }}>
+        THIRUVASAL
+      </div>
+      <div style={{ fontSize: 16, color: "#9BB5E0", marginBottom: 8 }}>
+        React is working!
+      </div>
+      <div style={{ fontSize: 13, color: "#5A7AB5" }}>
+        AyyaAkilam Platform
+      </div>
+    </div>
+  );
+}
