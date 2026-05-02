1b904da06235d1dce0f4e8956a42c4ae08b2ae68
sethu1286-jpg <sethu1286@gmail.com>
2026-05-03 04:40:28 +0530

    Delete src/index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
