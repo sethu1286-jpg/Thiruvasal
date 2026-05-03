10e543622a5498b47c495a5a8e96dcc60895692a
sethu1286-jpg <sethu1286@gmail.com>
2026-05-03 05:58:17 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#0F1B3D", color: "white", padding: 40 }}>
+      <h1>App working</h1>
+      <p>If this appears, Vercel is fine and your previous App.js code is broken.</p>
+    </div>
+  );
+}
