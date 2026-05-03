2c692062730ef2d2e750af1f2ea44a37dae5cd93
sethu1286-jpg <sethu1286@gmail.com>
2026-05-03 06:07:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function App() {
   return (
-    <div style={{ minHeight: "100vh", background: "#0F1B3D", color: "white", padding: 40 }}>
-      <h1>App working</h1>
-      <p>If this appears, Vercel is fine and your previous App.js code is broken.</p>
+    <div style={{ minHeight: "100vh", background: "#0F1B3D", color: "#fff", padding: "40px" }}>
+      <h1>Thiruvasal app is working</h1>
+      <p>If you can see this, the problem is in your old imports or Firebase code.</p>
     </div>
   );
 }
