c5bfad7846f5f8f2761a99470939fd7957312d90
sethu1286-jpg <sethu1286@gmail.com>
2026-05-03 06:08:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />); React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.js";
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
